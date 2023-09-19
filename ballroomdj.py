b0180f77da4d9b22126024d39a41718d4fdfaef3
mce27 <matthewemerick1@gmail.com>
2023-09-19 12:13:47 -0400

    add the title and icon

--- ballroomdj.py
+++ ballroomdj.py
@@ -2,12 +2,21 @@ import time, backend
 from tkinter import *
 from tkinter import ttk
 from pygame import mixer
+from PIL import Image
+from PIL import ImageTk
 #https://tkdocs.com/
 api_link='http://navidrome.mce27.xyz/rest/stream?id=5a8524f33621b876715bb7160289917c&u=mce27&t=e122d5bb2a94badb87dd8df90de1873f&s=d54g6h&v=1.12.0&c=myapp'
 
 Mfont=["Comic sans MS", 20]
 
 root = Tk()
+
+#Sets the title and the icon
+root.title("Ballroom DJ!")
+ico = Image.open('imgs/logo.ico')
+photo = ImageTk.PhotoImage(ico)
+root.wm_iconphoto(False, photo)
+
 root.grid()
 frm = ttk.Frame(root,padding="20")
 frm.grid(column=0,row=0)
