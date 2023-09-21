8a69b686f1f0a79d377cd415958651002c3b4aad
mce27 <matthewemerick1@gmail.com>
2023-09-21 16:06:58 -0400

    Added buttons and text for the rounds

--- ballroomdj.py
+++ ballroomdj.py
@@ -20,20 +20,28 @@ root.grid()
 frm = ttk.Frame(root,padding="20")
 frm.grid(column=0,row=0)
 textVar = 'Press the button!'
-lab=ttk.Label(frm,text=textVar)
-    
+std_round_label = ttk.Label(frm,text="Run a std round",padding='10').grid(column=0,row=0)
+lat_round_label = ttk.Label(frm,text="Run a lat round",padding='10').grid(column=1,row=0)
+smo_round_label = ttk.Label(frm,text="Run a smo round",padding='10').grid(column=2,row=0)
+rhy_round_label = ttk.Label(frm,text="Run a rhy round",padding='10').grid(column=3,row=0)
+all_round_label = ttk.Label(frm,text="Run all the rounds!",padding='10').grid(column=0,row=2)
+  
 
 def playSong():
     start = time.time()
-    filepath = backend.api_request(api_link)
+    filepath = ''
     mixer.init()
     mixer.music.load(filepath)
     mixer.music.play()
     end = time.time()
     print('Recieved file in '+str(end-start)+' seconds')
 
-but=ttk.Button(frm, text="Play!", command=playSong)
-lab.pack()
-but.pack()
+std_round_but = ttk.Button(frm, text="Play!", command=playSong).grid(column=0,row=1)
+lat_round_but = ttk.Button(frm, text="Play!", command=playSong).grid(column=1,row=1)
+smo_round_but = ttk.Button(frm, text="Play!", command=playSong).grid(column=2,row=1)
+rhy_round_but = ttk.Button(frm, text="Play!", command=playSong).grid(column=3,row=1)
+all_round_but = ttk.Button(frm, text="Play!", command=playSong).grid(column=0,row=3)
+#lab.pack()
+#but.pack()
 
 root.mainloop()
