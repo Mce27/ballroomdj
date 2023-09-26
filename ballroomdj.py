80e2e8b285b496a5732d7b5e78f259325c420968
mce27 <matthewemerick1@gmail.com>
2023-09-26 11:12:32 -0400

    Added all buttons and labels for rounds, style shuffle, and dance shuffle

--- ballroomdj.py
+++ ballroomdj.py
@@ -22,13 +22,50 @@ root.grid()
 frm = ttk.Frame(root,padding="20")
 frm.grid(column=0,row=0)
 statusVar = StringVar(frm)
-std_round_label = ttk.Label(frm,text="Run a std round",padding='10').grid(column=0,row=0)
-lat_round_label = ttk.Label(frm,text="Run a lat round",padding='10').grid(column=1,row=0)
-smo_round_label = ttk.Label(frm,text="Run a smo round",padding='10').grid(column=2,row=0)
-rhy_round_label = ttk.Label(frm,text="Run a rhy round",padding='10').grid(column=3,row=0)
-all_round_label = ttk.Label(frm,text="Run all the rounds!",padding='10').grid(column=0,row=2)
+
+rounds_label = ttk.Label(frm,text="Rounds:",padding='10').grid(column=0,row=0)
+smo_round_but = ttk.Button(frm, text="Smooth!", command=lambda:playThreadedRound('smo')).grid(column=0,row=1)
+std_round_but = ttk.Button(frm, text="Standard!", command=lambda:playThreadedRound('std')).grid(column=0,row=2)
+rhy_round_but = ttk.Button(frm, text="Rhythm!", command=lambda:playThreadedRound('rhy')).grid(column=0,row=3)
+lat_round_but = ttk.Button(frm, text="Latin!", command=lambda:playThreadedRound('lat')).grid(column=0,row=4)
+all_round_but = ttk.Button(frm, text="All!", command=lambda:playThreadedRound('all')).grid(column=0,row=5)
+
+shuffle_style_label = ttk.Label(frm,text="Shuffle Style",padding='10').grid(column=1,row=0)
+smo_shuffle_but = ttk.Button(frm, text="Smooth!", command=lambda:shuffleStyle('smo')).grid(column=1,row=1)
+std_shuffle_but = ttk.Button(frm, text="Standard!", command=lambda:shuffleStyle('std')).grid(column=1,row=2)
+rhy_shuffle_but = ttk.Button(frm, text="Rhythm!", command=lambda:shuffleStyle('rhy')).grid(column=1,row=3)
+lat_shuffle_but = ttk.Button(frm, text="Latin!", command=lambda:shuffleStyle('lat')).grid(column=1,row=4)
+all_shuffle_but = ttk.Button(frm, text="All!", command=lambda:shuffleStyle('all')).grid(column=1,row=5)
+
+shuffle_dance_label = ttk.Label(frm,text="Shuffle Dance",padding='10').grid(column=2,row=0)
+smo_waltz_shuffle_but = ttk.Button(frm, text="Waltz!", command=lambda:shuffleDance('smo','waltz')).grid(column=2,row=1)
+smo_tango_shuffle_but = ttk.Button(frm, text="Tango!", command=lambda:shuffleDance('smo','tango')).grid(column=2,row=1)
+smo_vwaltz_shuffle_but = ttk.Button(frm, text="VWaltz!", command=lambda:shuffleDance('smo','vwaltz')).grid(column=2,row=1)
+smo_foxtrot_shuffle_but = ttk.Button(frm, text="Foxtrot!", command=lambda:shuffleDance('smo','foxtrot')).grid(column=2,row=1)
+
+std_waltz_shuffle_but = ttk.Button(frm, text="Waltz!", command=lambda:shuffleDance('std','waltz')).grid(column=3,row=2)
+std_tango_shuffle_but = ttk.Button(frm, text="Tango!", command=lambda:shuffleDance('std','tango')).grid(column=3,row=2)
+std_vwaltz_shuffle_but = ttk.Button(frm, text="VWaltz!", command=lambda:shuffleDance('std','vwaltz')).grid(column=3,row=2)
+std_foxtrot_shuffle_but = ttk.Button(frm, text="Foxtrot!", command=lambda:shuffleDance('std','foxtrot')).grid(column=3,row=2)
+std_quickstep_shuffle_but = ttk.Button(frm, text="Quickstep!", command=lambda:shuffleDance('std','quickstep')).grid(column=3,row=2)
+
+rhy_chacha_shuffle_but = ttk.Button(frm, text="Chacha!", command=lambda:shuffleDance('rhy','chacha')).grid(column=4,row=3)
+rhy_rumba_shuffle_but = ttk.Button(frm, text="Rumba!", command=lambda:shuffleDance('rhy','rumba')).grid(column=4,row=3)
+rhy_swing_shuffle_but = ttk.Button(frm, text="Swing!", command=lambda:shuffleDance('rhy','swing')).grid(column=4,row=3)
+rhy_bolero_shuffle_but = ttk.Button(frm, text="Bolero!", command=lambda:shuffleDance('rhy','bolero')).grid(column=4,row=3)
+rhy_mambo_shuffle_but = ttk.Button(frm, text="Mambo!", command=lambda:shuffleDance('rhy','mambo')).grid(column=4,row=3)
+
+lat_chacha_shuffle_but = ttk.Button(frm, text="Chacha!", command=lambda:shuffleDance('lat','chacha')).grid(column=5,row=4)
+lat_rumba_shuffle_but = ttk.Button(frm, text="Rumba!", command=lambda:shuffleDance('lat','rumba')).grid(column=5,row=4)
+lat_samba_shuffle_but = ttk.Button(frm, text="Samba!", command=lambda:shuffleDance('lat','samba')).grid(column=5,row=4)
+lat_jive_shuffle_but = ttk.Button(frm, text="Jive!", command=lambda:shuffleDance('lat','jive')).grid(column=5,row=4)
+
+"""
 status_label = ttk.Label(frm,textvariable=statusVar,padding='10').grid(column=1,row=2)
-  
+pause_but = ttk.Button(frm, text="Pause", command=pauseSong).grid(column=0,row=4)
+unpause_but = ttk.Button(frm, text="! Pause", command=resumeSong).grid(column=1,row=4)
+skip_but = ttk.Button(frm, text="Skip", command=skipSong).grid(column=3,row=4)
+"""
 
 def playSong():
     filepath = ''
@@ -105,14 +142,7 @@ def skipSong():
     PAUSED = False
     mixer.music.fadeout(1000)
 
-std_round_but = ttk.Button(frm, text="Play!", command=lambda:playThreadedRound('std')).grid(column=0,row=1)
-lat_round_but = ttk.Button(frm, text="Play!", command=lambda:playThreadedRound('lat')).grid(column=1,row=1)
-smo_round_but = ttk.Button(frm, text="Play!", command=lambda:playThreadedRound('smo')).grid(column=2,row=1)
-rhy_round_but = ttk.Button(frm, text="Play!", command=lambda:playThreadedRound('rhy')).grid(column=3,row=1)
-all_round_but = ttk.Button(frm, text="Play!", command=lambda:playThreadedRound('all')).grid(column=0,row=3)
-pause_but = ttk.Button(frm, text="Pause", command=pauseSong).grid(column=0,row=4)
-unpause_but = ttk.Button(frm, text="! Pause", command=resumeSong).grid(column=1,row=4)
-skip_but = ttk.Button(frm, text="Skip", command=skipSong).grid(column=3,row=4)
+
 
 statusVar.set("Setting up...")
 backend.setup()
