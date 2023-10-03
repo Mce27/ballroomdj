e4781cac3a2898377afe6fc6dba3018e84bf7cc4
mce27 <matthewemerick1@gmail.com>
2023-10-03 11:58:10 -0400

    changed backend to pull api values from a txt

--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
 import os,requests,random
 from bs4 import BeautifulSoup
-api_link='http://ballroom.mce27.xyz/rest/stream?id={id}&format=mp3&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp'
+URL = ''
+token = ''
+user = ''
+salt = ''
 dances = {"smo":["waltz","tango","vwaltz","foxtrot"],
           "std":["waltz","tango","vwaltz","foxtrot","quickstep"],
           "rhy":["chacha","rumba","swing","bolero","mambo"],
@@ -11,7 +14,7 @@ def round_request(style:str):
     """
     pulls playlists from server and downloads one song for each dance in the style
     """
-    playlists = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylists?u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+    playlists = requests.get(f'http://{URL}/rest/getPlaylists?u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
     soup = BeautifulSoup(playlists.content,features='lxml')
     playlist_list = soup.find_all("playlist")
     playlist_dict = {}
@@ -19,12 +22,12 @@ def round_request(style:str):
         if playlist['name'].startswith(style) or style == 'all':
             playlist_dict[playlist['name']] = playlist['id']
     for cat in playlist_dict.keys():
-        content_of_playlist = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylist?id={playlist_dict[cat]}&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+        content_of_playlist = requests.get(f'http://{URL}/rest/getPlaylist?id={playlist_dict[cat]}&u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
         content_of_playlist = BeautifulSoup(content_of_playlist.content,features='lxml')
         songs = content_of_playlist.find_all('entry')
         i = random.randrange(0,len(songs)) #random int to grab a song
         song_to_dl = songs[i]['id']
-        req = requests.get(f'http://ballroom.mce27.xyz/rest/stream?id={song_to_dl}&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&format=mp3&s=ow130p2&v=1.12.0&c=myapp')
+        req = requests.get(f'http://{URL}/rest/stream?id={song_to_dl}&u={user}&t={token}&format=mp3&s={salt}&v=1.12.0&c=myapp')
         with open(f'music/{cat[:3]}/{cat[3:]}/{songs[i]["title"]}.mp3','wb') as file:
             file.write(req.content)
 
@@ -40,7 +43,7 @@ def get_random_style_song(style:str):
     dances_list = dances[style]
     i = random.randrange(0,len(dances_list)) #random int to grab a dance
     dance_to_get = dances_list[i]
-    playlists = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylists?u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+    playlists = requests.get(f'http://{URL}/rest/getPlaylists?u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
     soup = BeautifulSoup(playlists.content,features='lxml')
     playlist_list = soup.find_all("playlist")
     playlist_dict = {}
@@ -48,7 +51,7 @@ def get_random_style_song(style:str):
     for playlist in playlist_list:
         playlist_dict[playlist['name']] = playlist['id']
 
-    content_of_playlist = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylist?id={playlist_dict[style + dance_to_get]}&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+    content_of_playlist = requests.get(f'http://{URL}/rest/getPlaylist?id={playlist_dict[style + dance_to_get]}&u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
     content_of_playlist = BeautifulSoup(content_of_playlist.content,features='lxml')
     songs = content_of_playlist.find_all('entry')
     i = random.randrange(0,len(songs)) #random int to grab a song
@@ -62,7 +65,7 @@ def get_random_dance_song(style:str,dance:str):
     Downloads a random song from a certain dance and style
     Returns the filepath
     """
-    playlists = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylists?u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+    playlists = requests.get(f'http://{URL}/rest/getPlaylists?u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
     soup = BeautifulSoup(playlists.content,features='lxml')
     playlist_list = soup.find_all("playlist")
     playlist_dict = {}
@@ -70,7 +73,7 @@ def get_random_dance_song(style:str,dance:str):
     for playlist in playlist_list:
         playlist_dict[playlist['name']] = playlist['id']
 
-    content_of_playlist = requests.get(f'http://ballroom.mce27.xyz/rest/getPlaylist?id={playlist_dict[style + dance]}&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&s=ow130p2&v=1.12.0&c=myapp')
+    content_of_playlist = requests.get(f'http://{URL}/rest/getPlaylist?id={playlist_dict[style + dance]}&u={user}&t={token}&s={salt}&v=1.12.0&c=myapp')
     content_of_playlist = BeautifulSoup(content_of_playlist.content,features='lxml')
     songs = content_of_playlist.find_all('entry')
     i = random.randrange(0,len(songs)) #random int to grab a song
@@ -80,7 +83,7 @@ def get_random_dance_song(style:str,dance:str):
     return filepath, song_to_dl['title']
 
 def get_song(id:str,out_path:str):
-    song = requests.get(f'http://ballroom.mce27.xyz/rest/stream?id={id}&u=ballroom&t=32fc4daf799d520e6701b60cdb3178af&format=mp3&s=ow130p2&v=1.12.0&c=myapp')
+    song = requests.get(f'http://{URL}/rest/stream?id={id}&u={user}&t={token}&format=mp3&s={salt}&v=1.12.0&c=myapp')
     try:
         with open(out_path,'wb') as file:
             file.write(song.content)
@@ -108,5 +111,15 @@ def setup():
                 for dance in dances[cat]:#see if dir for each dance exists
                     if not os.path.exists(f'music/{cat}/{dance}'):
                         os.mkdir(f'music/{cat}/{dance}')
-    #now that fs is in place, time to dl music
-    #api_request()
+    # time to get user creds
+    if not os.path.exists('config.txt'):
+        print("Please put your credentials in a file named config.txt\n"\
+              "They should be in the form:\n URL\nuser\ntoken\nsalt\n"\
+              "This program is built to access a Subsonic api, namely Navidrome."\
+              "Refer to the 'Authentication' section in http://www.subsonic.org/pages/api.jsp for more information"
+              )
+    else:
+        global URL, user, token, salt
+        with open("config.txt") as file:
+            data = file.read()
+            URL, user, token, salt = data.split('\n')
